475250ef9807a5d43ce322c620e68d1a6eec0b9e
Konata69 <shaykinspicewolf@gmail.com>
2020-05-22 23:08:51 +0400

    test basic func

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
-func main() {
-
-}
-
-// Merge2Channels below
-/*
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
+	for i := 0; i < n; i++ {
+		x1, x2 := <-in1, <-in2
+		out <- f(x1) + f(x2)
+	}
 }
-*/
