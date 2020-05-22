f2746dadf87d19bec85b24b2a6bae6113792fdc7
Rinat Almakhov <gasoid@gmail.com>
2020-05-22 11:39:55 +0400

    added first tests

--- main.go
+++ main.go
@@ -4,8 +4,38 @@ func main() {
 
 }
 
-// Merge2Channels below
-/*
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
+	go func(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int) {
+		done := make(chan bool, n*2)
+		results := make([]*int, n*2)
+
+		go func() {
+			nDone := 0
+			for <-done {
+				for i := nDone; i < n; i++ {
+					if results[i] != nil && results[i+n] != nil {
+						out <- (*results[i] + *results[i+n])
+						if nDone++; nDone == n {
+							return
+						}
+					} else {
+						break
+					}
+				}
+			}
+		}()
+		input := func(ch <-chan int, results []*int) {
+			for i := 0; i < n; i++ {
+				x := <-ch
+				go func(i int, x int) {
+					result := f(x)
+					results[i] = &result
+					done <- true
+				}(i, x)
+			}
+		}
+		go input(in1, results[:n])
+		go input(in2, results[n:])
+
+	}(f, in1, in2, out)
 }
-*/
